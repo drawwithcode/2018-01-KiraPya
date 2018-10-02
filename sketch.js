0bb83c285e4692c70103d3107ed1625184808f16
KiraPya <43505670+KiraPya@users.noreply.github.com>
2018-10-02 08:19:36 +0200

    assignment1

--- sketch.js
+++ sketch.js
@@ -1,23 +1,146 @@
-function setup() {
-	// define artboard size
-	createCanvas(500,500);
-	// Set here the values that are not changing all over the sketch
-	strokeWeight(2);
-	stroke(170);
+function preload(){
+  // put preload code here
 }
 
+function setup() {
+  // put setup code here
+
+  background(174);
+  colorMode(HSB);
+
+}
+
+var x=20;
+var y=60;
+var speed=2;
+
 function draw() {
-	background(21);
+  // put drawing code here
+  createCanvas(800,600);
+  colorMode(RGB);
 
-	//Neck
-	line(250, 285, 250, 345);
+  //ellipse
+   noStroke();
+   fill(0,0,0);
+   ellipse(x, y, 70, 70);
+   x=x+speed;
 
-	//Wheel
-	ellipse(250, 465, 70, 70);
+ //line
+ strokeWeight(1);
+ stroke(0,0,0);
+ line(230, 38, 123, 21);
 
-	//Body
-	rect(200, 325, 100, 140);
+ //line2
+ strokeWeight(1);
+ stroke(0,0,0);
+ line(205, 177, 55, 15);
 
-	//Head
-	ellipse(250, 240, 90, 90);
+ //line2
+ strokeWeight(1);
+ stroke(0,0,0);
+ line(245, 137, 23, 19);
+
+
+  //legL
+  strokeWeight(1);
+  stroke(255,255,255);
+   fill(0,0,0);
+   rect(386, 303, 10, 247, 30);
+
+   //legR
+   strokeWeight(1);
+   stroke(255,255,255);
+    fill(0,0,0);
+    rect(312, 302, 10, 247, 30);
+
+    //handR1
+    strokeWeight(1);
+    stroke(255,255,255);
+     fill(0,0,0);
+     rect(402, 110, 8, 112);
+
+     //handR2
+     strokeWeight(1);
+     stroke(255,255,255);
+      fill(0,0,0);
+      rect(292, 110, 118, 8);
+
+      //fingerR
+      strokeWeight(1);
+      stroke(255,255,255);
+       fill(0,0,0);
+       rect(277, 110, 14, 5);
+
+       //fingerR1
+       strokeWeight(1);
+       stroke(255,255,255);
+        fill(0,0,0);
+        rect(271, 110, 5, 2);
+
+        //handL1
+        strokeWeight(1);
+        stroke(255,255,255);
+         fill(0,0,0);
+         rect(133, 222, 168, 8);
+
+         //handL2
+         strokeWeight(1);
+         stroke(255,255,255);
+          fill(0,0,0);
+          rect(133, 159, 8, 71);
+
+          //fingerL
+          strokeWeight(1);
+          stroke(255,255,255);
+           fill(0,0,0);
+           rect(133, 143, 5, 14);
+
+           //fingerL1
+           strokeWeight(1);
+           stroke(255,255,255);
+            fill(0,0,0);
+            rect(133, 137, 2, 6);
+
+
+            //Head
+            strokeWeight(1);
+            stroke(255,255,255);
+             fill(0,0,0);
+             rect(330, 166, 41, 41);
+
+             //mouse
+             strokeWeight(1);
+             stroke(255,255,255);
+              fill(0,0,0);
+             rect(342, 195, 15, 5);
+
+             //EyeL
+             strokeWeight(1);
+             stroke(255,255,255);
+              fill(0,0,0);
+              ellipse(339, 174, 8, 8);
+
+              //EyeR
+              strokeWeight(1);
+              stroke(255,255,255);
+               fill(0,0,0);
+               ellipse(356, 174, 8, 8);
+
+
+               //body
+
+               strokeWeight(1);
+               stroke(255,255,255);
+                fill(0,0,0);
+                rect(302, 206, 100, 100);
+
+
+
+
+   // rectangle
+   strokeWeight(12);
+   stroke(247, 137, 30);
+   noFill()
+
+
 }
